0cdb151b2730d4c7bbe182a411cd5052d403788c
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-11-01 13:38:14 +0100

    chore: file reformat for better understanding

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,64 +1,76 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 import styles from './styles.module.css';
 import { useRouter } from 'next/navigation';
 
 function Login() {
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const router = useRouter();
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const router = useRouter();
 
-    const submitLogin = (e: React.FormEvent<HTMLFormElement>): void => {
-        e.preventDefault();
+  const submitLogin = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
 
-        if (username.trim().length === 0 || password.trim().length === 0) {
-            alert('Username and password cannot be empty');
-        } else {
-            fetch('http://localhost:9090/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    username: username.trim(),
-                    password: password.trim()
-                }),
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        return response.json().then((data) => data);
-                    }
-                    throw {};
-                })
-                .then((body) => {
-                    sessionStorage.setItem('token', body.token);
-                    sessionStorage.setItem('userId', body.userId);
-                    router.push('/board');
-                    // navigate here
-                })
-                .catch(() => {
-                    alert('Error logging in');
-                });
-        }
-    };
+    if (username.trim().length === 0 || password.trim().length === 0) {
+      alert('Username and password cannot be empty');
+    } else {
+      fetch('http://localhost:9090/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: username.trim(),
+          password: password.trim()
+        })
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json().then((data) => data);
+          }
+          throw {};
+        })
+        .then((body) => {
+          sessionStorage.setItem('token', body.token);
+          sessionStorage.setItem('userId', body.userId);
+          router.push('/board');
+          // navigate here
+        })
+        .catch(() => {
+          alert('Error logging in');
+        });
+    }
+  };
 
-    return (<div className={styles.loginContainer}>
-<form onSubmit={submitLogin}>
-            <label className={styles.label}>
-                Username:
-                <input className={styles.input} type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </label>
-            <br />
-            <label className={styles.label}>
-                Password:
-                <input className={styles.input} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <br />
-            <button className={styles.button} type="submit">Login</button>
-        </form>
-
+  return (
+    <div className={styles.loginContainer}>
+      <form onSubmit={submitLogin}>
+        <label className={styles.label}>
+          Username:
+          <input
+            className={styles.input}
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label className={styles.label}>
+          Password:
+          <input
+            className={styles.input}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button className={styles.button} type="submit">
+          Login
+        </button>
+      </form>
     </div>
-            );
+  );
 }
 
 export default Login;
